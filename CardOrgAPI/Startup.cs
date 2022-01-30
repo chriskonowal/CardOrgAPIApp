38e0a5e6b29faf91ca9446b32dd73fe1db0c4c70
Chris Konowal <chriskonowal@gmail.com>
2022-01-30 08:53:25 -0500

    Cleanup

--- CardOrgAPI/Startup.cs
+++ CardOrgAPI/Startup.cs
@@ -1,29 +1,19 @@
 using CardOrgAPI.Contexts;
-using CardOrgAPI.Extensions;
 using CardOrgAPI.Helpers;
 using CardOrgAPI.Interfaces.Repositories;
 using CardOrgAPI.Interfaces.Servics;
 using CardOrgAPI.Repositories;
 using CardOrgAPI.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CardOrgAPI
 {
