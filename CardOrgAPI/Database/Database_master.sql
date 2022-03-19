4cc3676806178ba5b467c3fa096558e9c3a8c95e
Chris Konowal <chriskonowal@gmail.com>
2022-03-19 13:09:04 -0400

    Updating database creation file

--- CardOrgAPI/Database/Database_master.sql
+++ CardOrgAPI/Database/Database_master.sql
@@ -1,4 +1,4 @@
-USE [CardOrgTest]
+USE [CardOrg]
 GO
 
 /****** Object:  Table [dbo].[Years]    Script Date: 3/18/2022 5:51:54 PM ******/
@@ -9,84 +9,87 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-IF OBJECT_ID('dbo.FK_Cards_GradeCompanyId') IS NOT NULL AND OBJECT_ID(N'dbo.Cards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK_Card_GradeCompanyId') IS NOT NULL AND OBJECT_ID(N'dbo.Card') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.Cards DROP CONSTRAINT FK_Card_GradeCompanyId;
+	ALTER TABLE dbo.Card DROP CONSTRAINT FK_Card_GradeCompanyId;
 END
 
-IF OBJECT_ID('dbo.FK_Cards_LocationId') IS NOT NULL AND OBJECT_ID(N'dbo.Cards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK_Card_LocationId') IS NOT NULL AND OBJECT_ID(N'dbo.Card') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.Cards DROP CONSTRAINT FK_Card_LocationId;
+	ALTER TABLE dbo.Card DROP CONSTRAINT FK_Card_LocationId;
 END
 
-IF OBJECT_ID('dbo.FK_Cards_SportId') IS NOT NULL AND OBJECT_ID(N'dbo.Cards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK_Card_SportId') IS NOT NULL AND OBJECT_ID(N'dbo.Card') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.Cards DROP CONSTRAINT FK_Card_SportId;
+	ALTER TABLE dbo.Card DROP CONSTRAINT FK_Card_SportId;
 END
 
-IF OBJECT_ID('dbo.FK_Cards_YearId') IS NOT NULL AND OBJECT_ID(N'dbo.Cards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK_Card_YearId') IS NOT NULL AND OBJECT_ID(N'dbo.Card') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.Cards DROP CONSTRAINT FK_Card_YearId;
+	ALTER TABLE dbo.Card DROP CONSTRAINT FK_Card_YearId;
 END
 
-IF OBJECT_ID('dbo.FK_Cards_SetId') IS NOT NULL AND OBJECT_ID(N'dbo.Cards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK_Card_SetId') IS NOT NULL AND OBJECT_ID(N'dbo.Card') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.Cards DROP CONSTRAINT FK_Card_SetId;
+	ALTER TABLE dbo.Card DROP CONSTRAINT FK_Card_SetId;
 END
 
-IF OBJECT_ID('dbo.FK_PlayerCard_CardId') IS NOT NULL AND OBJECT_ID(N'dbo.PlayerCards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK_PlayerCard_CardId') IS NOT NULL AND OBJECT_ID(N'dbo.PlayerCard') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.PlayerCards DROP CONSTRAINT FK_PlayerCard_CardId;
+	ALTER TABLE dbo.PlayerCard DROP CONSTRAINT FK_PlayerCard_CardId;
 END
 
-IF OBJECT_ID('dbo.FK__PlayerCard_PlayerId') IS NOT NULL AND OBJECT_ID(N'dbo.PlayerCards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK__PlayerCard_PlayerId') IS NOT NULL AND OBJECT_ID(N'dbo.PlayerCard') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.PlayerCards DROP CONSTRAINT FK__PlayerCard_PlayerId;
+	ALTER TABLE dbo.PlayerCard DROP CONSTRAINT FK__PlayerCard_PlayerId;
 END
 
-IF OBJECT_ID('dbo.FK_TeamCards_CardId') IS NOT NULL AND OBJECT_ID(N'dbo.TeamCards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK_TeamCards_CardId') IS NOT NULL AND OBJECT_ID(N'dbo.TeamCard') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.TeamCards DROP CONSTRAINT FK_TeamCards_CardId;
+	ALTER TABLE dbo.TeamCard DROP CONSTRAINT FK_TeamCards_CardId;
 END
 
-IF OBJECT_ID('dbo.FK_TeamCards_TeamId') IS NOT NULL AND OBJECT_ID(N'dbo.TeamCards') IS NOT NULL  
+IF OBJECT_ID('dbo.FK_TeamCards_TeamId') IS NOT NULL AND OBJECT_ID(N'dbo.TeamCard') IS NOT NULL  
 BEGIN
-	ALTER TABLE dbo.TeamCards DROP CONSTRAINT FK_TeamCards_TeamId;
+	ALTER TABLE dbo.TeamCard DROP CONSTRAINT FK_TeamCards_TeamId;
 END
 
 
-IF OBJECT_ID(N'dbo.Years') IS NOT NULL  
-   DROP TABLE [dbo].[Years];
+IF OBJECT_ID(N'dbo.Year') IS NOT NULL  
+   DROP TABLE [dbo].[Year];
 
-IF OBJECT_ID(N'dbo.Teams') IS NOT NULL  
-   DROP TABLE [dbo].[Teams];
+IF OBJECT_ID(N'dbo.Team') IS NOT NULL  
+   DROP TABLE [dbo].[Team];
 
-IF OBJECT_ID(N'dbo.Sports') IS NOT NULL  
-   DROP TABLE [dbo].[Sports];
+IF OBJECT_ID(N'dbo.Sport') IS NOT NULL  
+   DROP TABLE [dbo].[Sport];
 
-IF OBJECT_ID(N'dbo.Sets') IS NOT NULL  
-   DROP TABLE [dbo].[Sets];
+IF OBJECT_ID(N'dbo.Set') IS NOT NULL  
+   DROP TABLE [dbo].[Set];
 
-IF OBJECT_ID(N'dbo.Players') IS NOT NULL  
-   DROP TABLE [dbo].[Players];
+IF OBJECT_ID(N'dbo.Player') IS NOT NULL  
+   DROP TABLE [dbo].[Player];
 
-IF OBJECT_ID(N'dbo.Locations') IS NOT NULL  
-   DROP TABLE [dbo].[Locations];
+IF OBJECT_ID(N'dbo.Location') IS NOT NULL  
+   DROP TABLE [dbo].[Location];
 
-IF OBJECT_ID(N'dbo.[GradeCompanies]') IS NOT NULL  
-   DROP TABLE [dbo].[GradeCompanies];
+IF OBJECT_ID(N'dbo.[GradeCompany]') IS NOT NULL  
+   DROP TABLE [dbo].[GradeCompany];
 
-IF OBJECT_ID(N'dbo.[Cards]') IS NOT NULL  
-   DROP TABLE [dbo].[Cards];
+IF OBJECT_ID(N'dbo.[Card]') IS NOT NULL  
+   DROP TABLE [dbo].[Card];
 
-IF OBJECT_ID(N'dbo.[PlayerCards]') IS NOT NULL  
-   DROP TABLE [dbo].[PlayerCards];
+IF OBJECT_ID(N'dbo.[PlayerCard]') IS NOT NULL  
+   DROP TABLE [dbo].[PlayerCard];
 
-IF OBJECT_ID(N'dbo.[TeamCards]') IS NOT NULL  
-   DROP TABLE [dbo].TeamCards;
+IF OBJECT_ID(N'dbo.[TeamCard]') IS NOT NULL  
+   DROP TABLE [dbo].TeamCard;
+
+IF OBJECT_ID(N'dbo.[SearchSort]') IS NOT NULL  
+   DROP TABLE [dbo].SearchSort;
    
 
-CREATE TABLE [dbo].[Years](
+CREATE TABLE [dbo].[Year](
 	[YearId] [int] IDENTITY(1,1) NOT NULL,
 	[BeginningYear] [int] NOT NULL,
 	[EndingYear] [int] NOT NULL,
@@ -97,7 +100,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
-CREATE TABLE [dbo].[Teams](
+CREATE TABLE [dbo].[Team](
 	[TeamId] [int] IDENTITY(1,1) NOT NULL,
 	[City] [nvarchar](254) NOT NULL,
 	[Name] [nvarchar](254) NOT NULL,
@@ -107,7 +110,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
-CREATE TABLE [dbo].[Sports](
+CREATE TABLE [dbo].[Sport](
 	[SportId] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](254) NOT NULL,
 PRIMARY KEY CLUSTERED 
@@ -116,7 +119,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
-CREATE TABLE [dbo].[Sets](
+CREATE TABLE [dbo].[Set](
 	[SetId] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](254) NOT NULL,
 PRIMARY KEY CLUSTERED 
@@ -125,7 +128,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
-CREATE TABLE [dbo].[Players](
+CREATE TABLE [dbo].[Player](
 	[PlayerId] [int] IDENTITY(1,1) NOT NULL,
 	[FirstName] [nvarchar](254) NOT NULL,
 	[LastName] [nvarchar](254) NOT NULL,
@@ -136,7 +139,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
-CREATE TABLE [dbo].[Locations](
+CREATE TABLE [dbo].[Location](
 	[LocationId] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](254) NOT NULL,
 PRIMARY KEY CLUSTERED 
@@ -145,7 +148,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
-CREATE TABLE [dbo].[GradeCompanies](
+CREATE TABLE [dbo].[GradeCompany](
 	[GradeCompanyId] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](254) NOT NULL,
 PRIMARY KEY CLUSTERED 
@@ -154,7 +157,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 
-CREATE TABLE [dbo].[Cards](
+CREATE TABLE [dbo].[Card](
 	[CardId] [int] IDENTITY(1,1) NOT NULL,
 	[CardDescription] [nvarchar](254) NOT NULL,
 	[CardNumber] [nvarchar](254) NOT NULL,
@@ -189,56 +192,56 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[Cards]  WITH CHECK ADD FOREIGN KEY([CardId])
-REFERENCES [dbo].[Cards] ([CardId])
+ALTER TABLE [dbo].[Card]  WITH CHECK ADD FOREIGN KEY([CardId])
+REFERENCES [dbo].[Card] ([CardId])
 GO
 
-ALTER TABLE [dbo].[Cards]  WITH CHECK ADD  CONSTRAINT [FK_Cards_GradeCompanyId] FOREIGN KEY([GradeCompanyId])
-REFERENCES [dbo].[GradeCompanies] ([GradeCompanyId])
+ALTER TABLE [dbo].[Card]  WITH CHECK ADD  CONSTRAINT [FK_Card_GradeCompanyId] FOREIGN KEY([GradeCompanyId])
+REFERENCES [dbo].[GradeCompany] ([GradeCompanyId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[Cards] CHECK CONSTRAINT [FK_Cards_GradeCompanyId]
+ALTER TABLE [dbo].[Card] CHECK CONSTRAINT [FK_Card_GradeCompanyId]
 GO
 
-ALTER TABLE [dbo].[Cards]  WITH CHECK ADD  CONSTRAINT [FK_Cards_LocationId] FOREIGN KEY([LocationId])
-REFERENCES [dbo].[Locations] ([LocationId])
+ALTER TABLE [dbo].[Card]  WITH CHECK ADD  CONSTRAINT [FK_Card_LocationId] FOREIGN KEY([LocationId])
+REFERENCES [dbo].[Location] ([LocationId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[Cards] CHECK CONSTRAINT [FK_Cards_LocationId]
+ALTER TABLE [dbo].[Card] CHECK CONSTRAINT [FK_Card_LocationId]
 GO
 
-ALTER TABLE [dbo].[Cards]  WITH CHECK ADD  CONSTRAINT [FK_Cards_SetId] FOREIGN KEY([SetId])
-REFERENCES [dbo].[Sets] ([SetId])
+ALTER TABLE [dbo].[Card]  WITH CHECK ADD  CONSTRAINT [FK_Card_SetId] FOREIGN KEY([SetId])
+REFERENCES [dbo].[Set] ([SetId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[Cards] CHECK CONSTRAINT [FK_Cards_SetId]
+ALTER TABLE [dbo].[Card] CHECK CONSTRAINT [FK_Card_SetId]
 GO
 
-ALTER TABLE [dbo].[Cards]  WITH CHECK ADD  CONSTRAINT [FK_Cards_SportId] FOREIGN KEY([SportId])
-REFERENCES [dbo].[Sports] ([SportId])
+ALTER TABLE [dbo].[Card]  WITH CHECK ADD  CONSTRAINT [FK_Card_SportId] FOREIGN KEY([SportId])
+REFERENCES [dbo].[Sport] ([SportId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[Cards] CHECK CONSTRAINT [FK_Cards_SportId]
+ALTER TABLE [dbo].[Card] CHECK CONSTRAINT [FK_Card_SportId]
 GO
 
-ALTER TABLE [dbo].[Cards]  WITH CHECK ADD  CONSTRAINT [FK_Cards_YearId] FOREIGN KEY([YearId])
-REFERENCES [dbo].[Years] ([YearId])
+ALTER TABLE [dbo].[Card]  WITH CHECK ADD  CONSTRAINT [FK_Card_YearId] FOREIGN KEY([YearId])
+REFERENCES [dbo].[Year] ([YearId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[Cards] CHECK CONSTRAINT [FK_Cards_YearId]
+ALTER TABLE [dbo].[Card] CHECK CONSTRAINT [FK_Card_YearId]
 GO
 
-CREATE TABLE [dbo].[PlayerCards](
+CREATE TABLE [dbo].[PlayerCard](
 	[PlayerCardId] [int] IDENTITY(1,1) NOT NULL,
 	[PlayerId] [int] NULL,
 	[CardId] [int] NULL,
@@ -249,25 +252,25 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[PlayerCards]  WITH CHECK ADD  CONSTRAINT [FK_PlayerCard_CardId] FOREIGN KEY([CardId])
-REFERENCES [dbo].[Cards] ([CardId])
+ALTER TABLE [dbo].[PlayerCard]  WITH CHECK ADD  CONSTRAINT [FK_PlayerCard_CardId] FOREIGN KEY([CardId])
+REFERENCES [dbo].[Card] ([CardId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[PlayerCards] CHECK CONSTRAINT [FK_PlayerCard_CardId]
+ALTER TABLE [dbo].[PlayerCard] CHECK CONSTRAINT [FK_PlayerCard_CardId]
 GO
 
-ALTER TABLE [dbo].[PlayerCards]  WITH CHECK ADD  CONSTRAINT [FK__PlayerCard_PlayerId] FOREIGN KEY([PlayerId])
-REFERENCES [dbo].[Players] ([PlayerId])
+ALTER TABLE [dbo].[PlayerCard]  WITH CHECK ADD  CONSTRAINT [FK__PlayerCard_PlayerId] FOREIGN KEY([PlayerId])
+REFERENCES [dbo].[Player] ([PlayerId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[PlayerCards] CHECK CONSTRAINT [FK__PlayerCard_PlayerId]
+ALTER TABLE [dbo].[PlayerCard] CHECK CONSTRAINT [FK__PlayerCard_PlayerId]
 GO
 
-CREATE TABLE [dbo].[TeamCards](
+CREATE TABLE [dbo].[TeamCard](
 	[TeamCardId] [int] IDENTITY(1,1) NOT NULL,
 	[TeamId] [int] NOT NULL,
 	[CardId] [int] NOT NULL,
@@ -278,22 +281,22 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[TeamCards]  WITH CHECK ADD  CONSTRAINT [FK_TeamCards_CardId] FOREIGN KEY([CardId])
-REFERENCES [dbo].[Cards] ([CardId])
+ALTER TABLE [dbo].[TeamCard]  WITH CHECK ADD  CONSTRAINT [FK_TeamCards_CardId] FOREIGN KEY([CardId])
+REFERENCES [dbo].[Card] ([CardId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[TeamCards] CHECK CONSTRAINT [FK_TeamCards_CardId]
+ALTER TABLE [dbo].[TeamCard] CHECK CONSTRAINT [FK_TeamCards_CardId]
 GO
 
-ALTER TABLE [dbo].[TeamCards]  WITH CHECK ADD  CONSTRAINT [FK_TeamCards_TeamId] FOREIGN KEY([TeamId])
-REFERENCES [dbo].[Teams] ([TeamId])
+ALTER TABLE [dbo].[TeamCard]  WITH CHECK ADD  CONSTRAINT [FK_TeamCards_TeamId] FOREIGN KEY([TeamId])
+REFERENCES [dbo].[Team] ([TeamId])
 ON UPDATE CASCADE
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[TeamCards] CHECK CONSTRAINT [FK_TeamCards_TeamId]
+ALTER TABLE [dbo].[TeamCard] CHECK CONSTRAINT [FK_TeamCards_TeamId]
 GO
 
 CREATE TABLE [dbo].[SearchSort](
